8323ae002a46823f0c2682372c5d0ca98df35c9f
Matthis <147597642+Sihtta@users.noreply.github.com>
2026-03-17 08:36:21 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'watchlist-app',
+  webDir: 'www'
+};
+
+export default config;
